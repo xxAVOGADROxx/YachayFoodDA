0e24f799662093ae5bea984a0038a064a870a5c7
Jose <jose.seraquive@gmail.com>
2018-04-16 17:21:32 -0500

    Solucion problema: static files

--- myYt/settings.py
+++ myYt/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.staticfiles',
-    'MyWebsite.books'
     'products',
     'cart',
     'orders',
